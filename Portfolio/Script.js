3d312d53d4494f2f7ff90931fedae45b49d19f47
RATNAKAR CHOUBEY <149227827+Ra1233-alt@users.noreply.github.com>
2026-01-20 00:32:30 +0530

    Add portfolio structure

---
+++ Portfolio/Script.js
@@ -0,0 +1,11 @@
+const cards = document.querySelectorAll('.card');
+
+window.addEventListener('scroll', () => {
+  cards.forEach(card => {
+    const pos = card.getBoundingClientRect().top;
+    if (pos < window.innerHeight - 100) {
+      card.style.opacity = 1;
+      card.style.transform = 'translateY(0)';
+    }
+  });
+});
